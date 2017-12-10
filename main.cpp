a12bad78eea6e265c2da803170ebba94423a66b0
suzuichi <suzuichiblog@gmail.com>
2017-12-10 13:18:51 +0900

    comment the flow of main function

--- main.cpp
+++ main.cpp
@@ -2,4 +2,9 @@
 using namespace std;
 
 int main(){
+    // read the xml file to load the images and set the feature points
+    // set parameters of the trainer
+    // generate the shape model
+    // test error
+    // save the model
 }
